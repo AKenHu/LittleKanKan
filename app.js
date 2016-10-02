e9d01503e92fa75eb19ae33092a8bfc54b6a45df
ken hu <kenhu4444@gmail.com>
2016-10-02 17:26:41 +0000

    test2

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ http.createServer(function(reg, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
     res.end('<p>Hello World</p>');
-}).listen(3000);
+}).listen(8080);
 console.log("HTTP Server is listenung at port 3000.")
